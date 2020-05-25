8fa037d2c5cc3d12a2351683e913143361bda207
GeekmasterK <ktheiss1990@gmail.com>
2020-05-25 11:21:56 -0700

    Camera following player. Installed cinemachine package to get it working.

---
+++ Assets/Scripts/CameraAttributes.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+/*
+ * This class stores the camera's global attribute variables
+ */
+public class CameraAttributes : MonoBehaviour
+{
+    [Tooltip("In ms^-1")] public float cameraMoveSpeed = 200f; // camera movement speed
+    [Tooltip("In ms^-1")] public float cameraLookSpeed = 150f; // camera look speed
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,51 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 /*
  * This class handles player movement based on directional inputs
  */
-
 public class PlayerMovement : MonoBehaviour
 {
+    public CharacterController characterController;
+    public Transform cam;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+
+    float turnSmoothVelocity;
+
+    PlayerAttributes playerAttributes;
+
+    PlayerInput playerInput;
+
+    void Awake()
+    {
+        playerAttributes = GetComponent<PlayerAttributes>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        Vector3 direction = new Vector3(playerInput.xDirection, 0f, playerInput.zDirection).normalized;
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+        if (direction.magnitude >= 0.1f)
+        {
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+
+    /*
     // player script variables
     PlayerAttributes playerAttributes;
     PlayerInput playerInput;
@@ -28,7 +68,7 @@ public class PlayerMovement : MonoBehaviour
 
     /*
      * Move the player based on directional inputs
-     */
+     
     void MovePlayer()
     {
         // horizontal and forward movement over time
@@ -38,19 +78,19 @@ public class PlayerMovement : MonoBehaviour
         // if the player's input is diagonal, move diagonally in that direction
         if(xMove != 0f && zMove != 0f)
         {
-            rigidBody.velocity = new Vector3(xMove * playerAttributes.moveSpeed, 0f, zMove * playerAttributes.moveSpeed);
+            rigidBody.velocity = new Vector3(xMove * playerAttributes.playerMoveSpeed, 0f, zMove * playerAttributes.playerMoveSpeed);
         }
 
         // if the player's input is left or right, move in that direction
         if(xMove != 0f && zMove == 0f)
         {
-            rigidBody.velocity = new Vector3(xMove * playerAttributes.moveSpeed, 0f, 0f);
+            rigidBody.velocity = new Vector3(xMove * playerAttributes.playerMoveSpeed, 0f, 0f);
         }
 
         // if the player's input is up or down, move forward on up input, and backward on down input
         if (xMove == 0f && zMove != 0f)
         {
-            rigidBody.velocity = new Vector3(0f, 0f, zMove * playerAttributes.moveSpeed);
+            rigidBody.velocity = new Vector3(0f, 0f, zMove * playerAttributes.playerMoveSpeed);
         }
-    }
+    }*/
 }
