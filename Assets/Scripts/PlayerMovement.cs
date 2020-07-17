231cf5cf7d29f3931645e86d3b4817760dc60e30
Tarsonis\Kevin <ktheiss1990@gmail.com>
2020-07-17 10:55:59 -0700

    Reverted player movement back to Rigidbody and collider physics. TODO: restore camera follow functionality.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,32 +10,32 @@ public class PlayerMovement : MonoBehaviour
     private PlayerAttributes playerAttributes;
     private PlayerInput playerInput;
 
-    private Vector3 moveDirection;
-    public float gravityScale = 1f;
+    //private Vector3 moveDirection;
+    //public float gravityScale = 1f;
 
-    //Rigidbody rigidBody;
+    Rigidbody rigidBody;
 
-    public CharacterController controller;
+    //public CharacterController controller;
 
     [SerializeField] Transform cam;
-    //[SerializeField] float turnSmoothTime = 0.1f;
+    [SerializeField] float turnSmoothTime = 0.1f;
 
-    //float turnSmoothVelocity;
+    float turnSmoothVelocity;
 
     // Initialization
     void Awake()
     {
         playerAttributes = GetComponent<PlayerAttributes>();
         playerInput = GetComponent<PlayerInput>();
-        //rigidBody = GetComponent<Rigidbody>();
-        controller = GetComponent<CharacterController>();
+        rigidBody = GetComponent<Rigidbody>();
+        //controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         MovePlayer();
-        //RotatePlayer();
+        RotatePlayer();
     }
 
     /*
@@ -43,12 +43,12 @@ public class PlayerMovement : MonoBehaviour
      */
     void MovePlayer()
     {
-        moveDirection = new Vector3(playerInput.horizontal * playerAttributes.moveSpeed, moveDirection.y, playerInput.vertical * playerAttributes.moveSpeed);
+        //moveDirection = new Vector3(playerInput.horizontal * playerAttributes.moveSpeed, moveDirection.y, playerInput.vertical * playerAttributes.moveSpeed);
 
         //moveDirection = (transform.forward * playerInput.vertical * playerAttributes.moveSpeed) + (transform.right * playerInput.horizontal * playerAttributes.moveSpeed);
         //moveDirection = moveDirection.normalized * playerAttributes.moveSpeed;
 
-        if(controller.isGrounded && moveDirection.y < 0f)
+        /*if(controller.isGrounded && moveDirection.y < 0f)
         {
             moveDirection.y = 0f;
         }
@@ -59,10 +59,12 @@ public class PlayerMovement : MonoBehaviour
         }
 
         moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
-        controller.Move(moveDirection * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);*/
+
+        rigidBody.velocity = Vector3.zero;
 
         // horizontal and forward movement over time
-        /*float xMove = playerInput.horizontal * Time.fixedDeltaTime;
+        float xMove = playerInput.horizontal * Time.fixedDeltaTime;
         float zMove = playerInput.vertical * Time.fixedDeltaTime;
 
         // if the player's input is diagonal, move diagonally in that direction
@@ -81,19 +83,19 @@ public class PlayerMovement : MonoBehaviour
         if (xMove == 0f && zMove != 0f)
         {
             rigidBody.velocity = new Vector3(0f, 0f, zMove * playerAttributes.moveSpeed);
-        }*/
+        }
     }
 
-    /*void RotatePlayer()
+    void RotatePlayer()
     {
         Vector3 lookDirection = new Vector3(playerInput.horizontal, 0f, playerInput.vertical).normalized;
 
-        float targetAngle = Mathf.Atan2(lookDirection.x, lookDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float targetAngle = Mathf.Atan2(lookDirection.x, lookDirection.z) * Mathf.Rad2Deg;
         float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
 
         if(lookDirection.magnitude >= 0.1f)
         {
             transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
         }
-    }*/
+    }
 }
