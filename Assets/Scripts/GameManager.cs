d196bac005cda66ad2516169566925c3e847fa1c
Tarsonis\Kevin <ktheiss1990@gmail.com>
2020-07-19 18:02:49 -0700

    Preload scene and global game manager added. Level reset trigger added. TODO: Add manual level reset and platforms.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Scene nextScene;
+    [HideInInspector] public Scene currentScene;
+    //public float nextSceneIndex;
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        LoadNextLevel();
+    }
+
+    public void LoadNextLevel()
+    { 
+        SceneManager.LoadScene(nextScene.handle);
+    }
+
+    public void ResetLevel()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+    }
+}
---
+++ Assets/Scripts/ResetTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetTrigger : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            gameManager.ResetLevel();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
